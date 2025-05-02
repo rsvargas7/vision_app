2668cce26d45c7f3ebb8694d5c4ca3e1f6931113
rsvargas7 <rsvargas.742@gmail.com>
2025-05-01 21:17:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ def encode_image(image_file):
     return base64.b64encode(image_file.getvalue()).decode("utf-8")
 
 
-st.set_page_config(page_title="Analisis de imagen", layout="centered", initial_sidebar_state="collapsed")
+st.set_page_config(page_title="Procesamiento de imagen", layout="centered", initial_sidebar_state="collapsed")
 # Streamlit page setup
-st.title("Análisis de Imagen:🤖🏞️")
-ke = st.text_input('Ingresa tu Clave')
+st.title("Procesamiento de Imagen")
+ke = st.text_input('Introduce tu clave')
 os.environ['OPENAI_API_KEY'] = ke
 
 
@@ -30,7 +30,7 @@ if uploaded_file:
         st.image(uploaded_file, caption=uploaded_file.name, use_container_width=True)
 
 # Toggle for showing additional details input
-show_details = st.toggle("Adiciona detalles sobre la imagen", value=False)
+show_details = st.toggle("Agrega información sobre la imagen", value=False)
 
 if show_details:
     # Text input for additional details about the image, shown only if toggle is True
@@ -40,12 +40,12 @@ if show_details:
     )
 
 # Button to trigger the analysis
-analyze_button = st.button("Analiza la imagen", type="secondary")
+analyze_button = st.button("Examina la imagen", type="secondary")
 
 # Check if an image has been uploaded, if the API key is available, and if the button has been pressed
 if uploaded_file is not None and api_key and analyze_button:
 
-    with st.spinner("Analizando ..."):
+    with st.spinner("Examinando ..."):
         # Encode the image
         base64_image = encode_image(uploaded_file)
     
